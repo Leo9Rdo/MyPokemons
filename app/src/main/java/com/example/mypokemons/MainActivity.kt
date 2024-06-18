4baa02e1e24914999cff985d34679b409c18a00c
John <muravei20004@mail.ru>
2024-06-19 01:30:03 +0300

    update

--- app/src/main/java/com/example/mypokemons/MainActivity.kt
+++ app/src/main/java/com/example/mypokemons/MainActivity.kt
@@ -4,34 +4,65 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mypokemons.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var  binding: ActivityMainBinding
+    private var currentIndex = 0
+
+    data class Pokemon(
+        val name: String,
+        val type: String,
+        val height: String,
+        val weight: String,
+        @DrawableRes val imageResId: Int
+    )
+
+    private val pokemons = listOf(
+        Pokemon("Butterfree", "Bug", "11 sm", "320 g", R.drawable.butterfree),
+        Pokemon("Charmeleon", "Fire", "11 sm", "190 g", R.drawable.charmeleon),
+        Pokemon("Pidgay", "Flying","3 sm", "18 g", R.drawable.pidgey),
+        Pokemon("Squirtle", "water","5 sm", "90 g", R.drawable.squirtle),
+        Pokemon("Wartortle", "water","10 sm", "150 g", R.drawable.wartortle),
+        Pokemon("Ivysaur", "poisin","10 sm", "130 g", R.drawable.ivysaur)
+
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
        binding.imgCard.setImageResource(R.drawable.butterfree)
-
+        val bundle = Bundle()
         binding.imgCard.setOnClickListener{
-            val intent = Intent(this,SecondActivity::class.java)
-            val bundle = Bundle()
-            intent.putExtra("name","butterfree")
-            intent.putExtra("type", "pokemon")
-            intent.putExtra("height", "23")
-            intent.putExtra("weight","42")
+            val Poke = pokemons[currentIndex]
+            val intent = Intent(this,SecondActivity::class.java).apply {
+                putExtra("name", Poke.name)
+                putExtra("type", Poke.type)
+                putExtra("height", Poke.height)
+                putExtra("weight", Poke.weight)
+                putExtra("image",Poke.imageResId)
 
-            intent.putExtras(bundle)
+            }
+           // intent.putExtras(bundle)
             startActivity(intent)
+        }
+        binding.btnNext.setOnClickListener {
+            currentIndex = (currentIndex + 1) % pokemons.size
+            update()
         }
     }
 
+
+    private fun update() {
+        val currentPokemon = pokemons[currentIndex]
+        binding.imgCard.setImageResource(currentPokemon.imageResId)
+    }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
--- app/src/main/java/com/example/mypokemons/SecondActivity.kt
+++ app/src/main/java/com/example/mypokemons/SecondActivity.kt
@@ -1,38 +1,51 @@
 package com.example.mypokemons
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.content.Intent
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mypokemons.databinding.ActivitySecondBinding
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.annotation.DrawableRes
 
 class SecondActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySecondBinding
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val extras = intent.extras
-        if (extras != null) {
-            val name = extras.getString("name")
-            val type = extras.getString("type")
-            val height = extras.getString("height")
-            val weight = extras.getString("weight")
+        intent?.let {
+            val name = it.getStringExtra("name")
+            val type = it.getStringExtra("type")
+            val height = it.getStringExtra("height")
+            val weight = it.getStringExtra("weight")
+            val imgRes= it.getIntExtra("image",0)
 
-            binding.txtName.setText("Name: " + name);
-            binding.txtType.setText("Type: " + type);
-            binding.txtHeight.setText("Height: " + height + " m");
-            binding.txtWidth.setText("Weight: " + weight + " kg");
-
-            binding.imageCard2.setImageResource(R.drawable.butterfree);
+            if (name != null && type != null && height != null && weight != null && imgRes != 0) {
+            with(binding) {
+                txtName.text = name
+                txtType.text= type
+                txtHeight.text=height
+                txtWidth.text=weight
+                imageCard2.setImageResource(imgRes)
+            }
+        }
+            else {
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+                finish()
+            }
         }
 
-
-        val btn_back2 : Button=this.findViewById<Button>(R.id.btn_back2)
-            btn_back2.setOnClickListener{
-                finish()
-
-            }
+        binding.btnBack2.setOnClickListener {
+            finish()
+        }
+    }
+    private fun ImageView.loadImage(@DrawableRes resId: Int) {
+        this.setImageResource(resId)
     }
 }
